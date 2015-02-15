0308291a1ac1d26dc35a32041fb1b35253ea92f2
SoyNerdito <soynerdito@gmail.com>
2015-02-15 12:58:06 -0400

    Removed un-used imports

--- src/com/soyblue/bluesound/MainActivity.java
+++ src/com/soyblue/bluesound/MainActivity.java
@@ -1,10 +1,7 @@
 package com.soyblue.bluesound;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
@@ -13,12 +10,6 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		return true;
 	}
 	
 	private void enableView( int resId, boolean enabled ){
